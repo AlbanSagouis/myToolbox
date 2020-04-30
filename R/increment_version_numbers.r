6cda5a4d9e40589eac0ffc971ef695221ed03d56
AlbanSagouis <25483578+AlbanSagouis@users.noreply.github.com>
2020-04-30 16:48:14 +0200

    Update of incVer

--- R/increment_version_numbers.r
+++ R/increment_version_numbers.r
@@ -1,17 +1,16 @@
 #' Increment version number of a package
 #'
 #' Reads DESCRIPTION from installed package 'pkg' and makes a new one in the specified
-#''folder'. Three options for 'increase' are "minor", "patch" and "dev" respectively
-#' incrementing the second, third and fourth elements of version 0.1.0.9012.
-#'
-#' Also updates the date to system date.
+#''folder'. Four options for 'increase' are "major", minor", "patch" and "dev"
+#' respectively incrementing the first to fourth element of version 0.1.0.9012.
+#' Also changes the date to system date.
 #'
 #' @param pkg a character vector containing complete
 #' @param folder description
 #' @param increase description
 #,
 #' @return NULL
-#' @details description. Originally posted by alko989 here https://stackoverflow.com/q/24209336. Hadley Wickham discusses when and how increment version numbers in his book http://r-pkgs.had.co.nz/release.html#release-version
+#' @details Originally posted by alko989 here https://stackoverflow.com/q/24209336. Hadley Wickham discusses when and how increment version numbers in his book http://r-pkgs.had.co.nz/release.html#release-version
 #'
 #' @author alko989, Alban Sagouis
 #'
@@ -20,33 +19,41 @@
 
 
 incVer <- function(pkg, folder=".", increase="dev"){
-
+   ## Read DESCRIPTION
+   dcf_colnames <- colnames(read.dcf(file=system.file("DESCRIPTION", package=pkg)))
+   dcf_colnames <- dcf_colnames[dcf_colnames != "Built"]
    f <- read.dcf(file=system.file("DESCRIPTION", package=pkg),
-                 fields=c("Package", "Type", "Title", "Version", "Date",
-                          "Author", "Maintainer", "Description", "License",
-                          "Depends", "Imports", "Suggests",
-                          "Encoding", "LazyData", "RoxygenNote"))
-   curVer <- package_version(f[4])
-   if(increase == "patch") {
-      curVer[[1,3]] <- ifelse(is.na(curVer$patchlevel), 1, curVer$patchlevel + 1)
+                 fields=dcf_colnames)
 
+   ## Increment version number
+   curVer <- package_version(f[4])
+   if(increase == "dev") {
+      curVer[[1,4]] <- ifelse(is.na(curVer[[1,4]]) | curVer[[1,4]] == 0, 9001, curVer[[1,4]] + 1)
+   } else if (increase == "patch") {
+      curVer[[1,3]] <- ifelse(is.na(curVer$patchlevel), 1, curVer$patchlevel + 1)
+      curVer[[1,4]] <- 0
    } else if (increase == "minor") {
       curVer[[1,2]] <- ifelse(is.na(curVer$minor), 1, curVer$minor + 1)
       curVer[[1,3]] <- 0
       curVer[[1,4]] <- 0
-   } else if (increase == "dev") {
-      curVer[[1,4]] <- ifelse(is.na(curVer[[1,4]]), 9001, curVer[[1,4]] + 1)
-
+   } else if (increase == "major") {
+      curVer[[1,1]] <- ifelse(is.na(curVer$major), 1, curVer$major + 1)
+      curVer[[1,2]] <- 0
+      curVer[[1,3]] <- 0
+      curVer[[1,4]] <- 0
    } else {
       stop(paste("Can not identify the increase argument: " , increase))
    }
 
    f[4] <- toString(curVer)
+
    ## Update also the date
    f[5] <- format (Sys.time(), "%Y-%m-%d")
+
+   ## Save
    write.dcf(f, file=paste(folder, "DESCRIPTION", sep="/"))
 
-   if(increase == 'minor' | increase == 'patch') print(
+   if(increase == 'major' | increase == 'minor' | increase == 'patch') print(
       "Consider updating README.md and NEWS.md."
    )
 
